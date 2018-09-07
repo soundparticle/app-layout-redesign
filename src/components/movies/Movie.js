f4c8d8eecb4f1ac4f9d539f8c35ef4d7f194a4ac
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-06 22:24:13 -0700

    cleaning up files a bit

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import styles from './movie.css';
-import logo from '../../assets/logo.png';
+// import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+// import { Link } from 'react-router-dom';
+// import styles from './movie.css';
+// import logo from '../../assets/logo.png';
 
-export default class Movie extends Component {
+// export default class Movie extends Component {
 
-  static propTypes = {
-    movie: PropTypes.object.isRequired
-  };
+//   static propTypes = {
+//     movie: PropTypes.object.isRequired
+//   };
 
-  render() {
-    const { movie } = this.props;
-    const { Title, Year, Poster, imdbID } = movie;
+//   render() {
+//     const { movie } = this.props;
+//     const { Title, Year, Poster, imdbID } = movie;
     
-    return (
-      <li className={styles.movie}>
-        <Link to={`/movies/${imdbID}`}>
-          {Poster !== 'N/A'
-            ? <img src={Poster}/>
-            : <img src={logo}/>}
-          <p><strong>{Title}</strong> ({Year})</p>
-        </Link>
-      </li>
-    );
-  }
-}
+//     return (
+//       <li className={styles.movie}>
+//         <Link to={`/movies/${imdbID}`}>
+//           {Poster !== 'N/A'
+//             ? <img src={Poster}/>
+//             : <img src={logo}/>}
+//           <p><strong>{Title}</strong> ({Year})</p>
+//         </Link>
+//       </li>
+//     );
+//   }
+// }
